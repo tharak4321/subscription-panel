7fd66dc3ac9ff014ec9d8a7b4cf3efd2116f3305
tharak4321 <75661483+tharak4321@users.noreply.github.com>
2026-06-04 14:21:00 +0530

    Update pay.js

--- pages/pay.js
+++ pages/pay.js
@@ -23,7 +23,7 @@ export default function Pay() {
         )}
 
         <img
-          src="https://i.ibb.co/rG2GscPp/IMG-20260506-212001-658.jpg"
+          src="https://i.ibb.co/Wv9ZMDn2/Google-Pay-QR-1.png"
           width="220"
           style={{borderRadius:10, margin:10}}
         />
